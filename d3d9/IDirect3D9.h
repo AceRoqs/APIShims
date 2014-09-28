a22251e355898b723ee77af4d254c093b5e1cab4
Toby Jones <ace_roqs@yahoo.com>
2014-09-27 19:20:43 -0700

    Match style changes.

--- d3d9/IDirect3D9.h
+++ d3d9/IDirect3D9.h
@@ -3,30 +3,30 @@
 
 struct IDirect3D9 : public IUnknown
 {
-    IFACEMETHOD(CheckDepthStencilMatch)(UINT Adapter, enum D3DDEVTYPE DeviceType, enum D3DFORMAT AdapterFormat, D3DFORMAT RenderTargetFormat, D3DFORMAT DepthStencilFormat) = 0;
-    IFACEMETHOD(CheckDeviceFormat)(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT AdapterFormat, DWORD Usage, enum D3DRESOURCETYPE RType, D3DFORMAT CheckFormat) = 0;
-    IFACEMETHOD(CheckDeviceFormatConversion)(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT SourceFormat, D3DFORMAT TargetFormat) = 0;
-    IFACEMETHOD(CheckDeviceMultiSampleType)(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT SurfaceFormat, BOOL Windowed, enum D3DMULTISAMPLE_TYPE MultiSampleType, _Out_ DWORD* pQualityLevels) = 0;
-    IFACEMETHOD(CheckDeviceType)(UINT Adapter, D3DDEVTYPE DeviceType, D3DFORMAT DisplayFormat, D3DFORMAT BackBufferFormat, BOOL Windowed) = 0;
-    IFACEMETHOD(CreateDevice)(UINT Adapter, D3DDEVTYPE DeviceType, HWND hFocusWindow, DWORD BehaviorFlags, _Inout_ struct D3DPRESENT_PARAMETERS* pPresentationParameters, _Outptr_ struct IDirect3DDevice9** ppReturnedDeviceInterface) = 0;
-    IFACEMETHOD(EnumAdapterModes)(UINT Adapter, D3DFORMAT Format, UINT Mode, _Out_ struct D3DDISPLAYMODE* pMode) = 0;
+    IFACEMETHOD(CheckDepthStencilMatch)(UINT adapter, enum D3DDEVTYPE device_type, enum D3DFORMAT adapter_format, D3DFORMAT render_target_format, D3DFORMAT depth_stencil_format) = 0;
+    IFACEMETHOD(CheckDeviceFormat)(UINT adapter, D3DDEVTYPE device_type, D3DFORMAT adapter_format, DWORD usage, enum D3DRESOURCETYPE resource_type, D3DFORMAT check_format) = 0;
+    IFACEMETHOD(CheckDeviceFormatConversion)(UINT adapter, D3DDEVTYPE device_type, D3DFORMAT source_format, D3DFORMAT target_format) = 0;
+    IFACEMETHOD(CheckDeviceMultiSampleType)(UINT adapter, D3DDEVTYPE device_type, D3DFORMAT surface_format, BOOL windowed, enum D3DMULTISAMPLE_TYPE multisample_type, _Out_ DWORD* quality_levels) = 0;
+    IFACEMETHOD(CheckDeviceType)(UINT adapter, D3DDEVTYPE device_type, D3DFORMAT display_format, D3DFORMAT back_buffer_format, BOOL windowed) = 0;
+    IFACEMETHOD(CreateDevice)(UINT adapter, D3DDEVTYPE device_type, HWND focus_window, DWORD behavior_flags, _Inout_ struct D3DPRESENT_PARAMETERS* presentation_parameters, _Outptr_ struct IDirect3DDevice9** device_interface) = 0;
+    IFACEMETHOD(EnumAdapterModes)(UINT adapter, D3DFORMAT format, UINT index, _Out_ struct D3DDISPLAYMODE* mode) = 0;
     IFACEMETHOD_(UINT, GetAdapterCount)() = 0;
-    IFACEMETHOD(GetAdapterDisplayMode)(UINT Adapter, _Out_ D3DDISPLAYMODE* pMode) = 0;
-    IFACEMETHOD(GetAdapterIdentifier)(UINT Adapter, DWORD Flags, _Out_ struct D3DADAPTER_IDENTIFIER9* pIdentifier) = 0;
-    IFACEMETHOD_(UINT, GetAdapterModeCount)(UINT Adapter, D3DFORMAT Format) = 0;
-    IFACEMETHOD_(HMONITOR, GetAdapterMonitor)(UINT Adapter) = 0;
-    IFACEMETHOD(GetDeviceCaps)(UINT Adapter, D3DDEVTYPE DeviceType, _Out_ struct D3DCAPS9* pCaps) = 0;
-    IFACEMETHOD(RegisterSoftwareDevice)(void* pInitializeFunction) = 0;
+    IFACEMETHOD(GetAdapterDisplayMode)(UINT adapter, _Out_ D3DDISPLAYMODE* mode) = 0;
+    IFACEMETHOD(GetAdapterIdentifier)(UINT adapter, DWORD flags, _Out_ struct D3DADAPTER_IDENTIFIER9* identifier) = 0;
+    IFACEMETHOD_(UINT, GetAdapterModeCount)(UINT adapter, D3DFORMAT format) = 0;
+    IFACEMETHOD_(HMONITOR, GetAdapterMonitor)(UINT adapter) = 0;
+    IFACEMETHOD(GetDeviceCaps)(UINT adapter, D3DDEVTYPE device_type, _Out_ struct D3DCAPS9* caps) = 0;
+    IFACEMETHOD(RegisterSoftwareDevice)(void* initialize_function) = 0;
 };
 
 struct IDirect3D9Ex : public IDirect3D9
 {
-    IFACEMETHOD(CreateDeviceEx)(UINT Adapter, D3DDEVTYPE DeviceType, _In_opt_ HWND hFocusWindow, DWORD BehaviorFlags, _Inout_ D3DPRESENT_PARAMETERS* pPresentationParameters,
-        _Out_ struct D3DDISPLAYMODEEX* pFullscreenDisplayMode, _Outptr_ struct IDirect3DDevice9Ex** ppReturnedDeviceInterface) = 0;
-    IFACEMETHOD(EnumAdapterModesEx)(UINT Adapter, _In_ const struct D3DDISPLAYMODEFILTER* pFilter, UINT Mode, _Out_ D3DDISPLAYMODEEX* pMode) = 0;
-    IFACEMETHOD(GetAdapterDisplayModeEx)(UINT Adapter, _Out_opt_ D3DDISPLAYMODEEX* pMode, _Out_opt_ enum D3DDISPLAYROTATION* pRotation) = 0;
-    IFACEMETHOD(GetAdapterLUID)(UINT Adapter, _Out_ LUID* pLUID) = 0;
-    IFACEMETHOD(GetAdapterModeCountEx)(UINT Adapter, _In_ const D3DDISPLAYMODEFILTER* pFilter) = 0;
+    IFACEMETHOD(CreateDeviceEx)(UINT adapter, D3DDEVTYPE device_type, _In_opt_ HWND focus_window, DWORD behavior_flags, _Inout_ D3DPRESENT_PARAMETERS* presentation_parameters,
+        _Out_ struct D3DDISPLAYMODEEX* full_screen_display_mode, _Outptr_ struct IDirect3DDevice9Ex** device_interface) = 0;
+    IFACEMETHOD(EnumAdapterModesEx)(UINT adapter, _In_ const struct D3DDISPLAYMODEFILTER* filter, UINT index, _Out_ D3DDISPLAYMODEEX* mode) = 0;
+    IFACEMETHOD(GetAdapterDisplayModeEx)(UINT adapter, _Out_opt_ D3DDISPLAYMODEEX* mode, _Out_opt_ enum D3DDISPLAYROTATION* rotation) = 0;
+    IFACEMETHOD(GetAdapterLUID)(UINT adapter, _Out_ LUID* luid) = 0;
+    IFACEMETHOD(GetAdapterModeCountEx)(UINT adapter, _In_ const D3DDISPLAYMODEFILTER* filter) = 0;
 };
 
 #endif
