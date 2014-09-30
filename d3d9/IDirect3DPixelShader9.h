9f1e30295b164331c6fe41db3c10e5e17c5397b4
Toby Jones <ace_roqs@yahoo.com>
2014-09-30 11:52:33 -0700

    Correct SAL.

--- d3d9/IDirect3DPixelShader9.h
+++ d3d9/IDirect3DPixelShader9.h
@@ -6,7 +6,7 @@
 struct IDirect3DPixelShader9 : public IUnknown
 {
     IFACEMETHOD(GetDevice)(_Outptr_ IDirect3DDevice9** ppDevice) = 0;
-    IFACEMETHOD(GetFunction)(_Out_writes_(*size) void* data, _Out_ UINT* size) = 0;
+    IFACEMETHOD(GetFunction)(_Out_writes_opt_(*size) void* data, _Out_ UINT* size) = 0;
 };
 
 #endif
